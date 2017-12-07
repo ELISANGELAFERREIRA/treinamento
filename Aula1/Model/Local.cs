f451cae7aadf97da17b1adc4b89f6c8a8fa3c794
Elisângela Ferreira <elisange.f@hotmail.com>
2017-12-07 20:31:32 -0200

    initial commit

---
+++ Aula1/Model/Local.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula1.Model
+{
+    class Local
+    {
+        public string Code { get; set; }
+
+        public int Size { get; set; }
+
+        public Local()
+           // : this("L1", 100)
+        {
+        }
+
+        public Local(string code, int size)
+        {
+            Code = code;
+            Size = size;
+        }
+
+        public virtual void Harvest() //colher
+        {
+
+        }
+
+    }
+
+    class Sector: Local
+    {
+        //public string Code { get; set; }
+       // public int Size { get; set; }
+
+        public Farm[] Farms { get; set; }
+
+        public Sector(String code, int size)
+            : base(code, size)
+        {
+            //Code = code;
+            //Size = size;
+        }
+
+        public override void Harvest()
+        {
+
+            Console.WriteLine("Harvest Sector {0}", Code);
+        } 
+
+    }
+
+
+    class Farm : Local
+    {
+        public Block[] Blocks { get; set; }
+
+        public override void Harvest()
+        {
+
+            Console.WriteLine("Harvest Farm {0}", Code);
+        }
+    }
+
+
+
+    class Block : Local
+    {
+        public Field[] Fields { get; set; }
+
+        public override void Harvest()
+        {
+
+            Console.WriteLine("Harvest Block {0}", Code);
+        }
+    }
+
+
+    class Field : Local
+    {
+        private string _culture;
+
+        public string Culture
+        {
+            get
+            {
+                return _culture;
+            }
+
+            set
+            {
+                _culture = value;
+            }
+        } 
+
+        public void Plant(string culture)
+        {
+            Culture = culture;
+        }
+
+        public override void Harvest()
+        {
+
+            Console.WriteLine("Harvest Field {0}", Code);
+        }
+    }
+}
---
+++ Aula1/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Aula1.Model;
+
+namespace Aula1
+{
+    class Program
+    {
+
+        public static void Print(object o)
+        {
+            Console.WriteLine(o.ToString());
+        }
+
+
+        public static void Show(Local local)
+        {
+            Console.WriteLine("{0} - {1}", local.Code, local.Size);
+            local.Harvest();
+
+        }
+
+        static void Main(string[] args)
+        {
+            /* Console.WriteLine("Hello World!");
+             Console.ReadKey();*/
+            int i = 10;
+            bool b = true;
+            string s = "Felipe";
+
+            Print(i);
+            Print(b);
+            Print(s);
+
+
+            Sector sectorA = new Sector("S1", 100);
+
+            Farm farm1 = new Farm();
+            farm1.Code = "F1";
+
+            Block block1 = new Block();
+            block1.Code = "B1";
+            block1.Size = 50;
+
+
+            Field field1 = new Field();
+            field1.Code = "FI1";
+            field1.Size = 10;
+            field1.Culture = "Suggar Cane";
+
+            
+            Show(sectorA);
+            Show(farm1);
+            Show(block1);
+            Show(field1);
+
+            Console.ReadKey();
+        
+        }
+
+   
+
+
+
+
+    }
+}
